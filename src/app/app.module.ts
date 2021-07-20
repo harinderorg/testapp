2a2417c728739b04fcfc6373ac23514c62f076d8
Your Name <you@example.com>
2021-07-20 10:22:49 +0530

    latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { TwitterConnect } from '@ionic-native/twitter-connect/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +11,12 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+   ,FirebaseAuthentication,
+   TwitterConnect
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { FirebaseAuthentication } from '@ionic-native/firebase-authentication/ngx';
+import { TwitterConnect } from '@ionic-native/twitter-connect/ngx';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private firebaseAuthentication: FirebaseAuthentication, private twitter: TwitterConnect) {}
 
+
+  login(){
+  //   this.firebaseAuthentication.signInWithTwitter('803837273708101632-3JoY83cLxaYj3whBWLBbZWOy4eSOsuq', 'hkVl70O8nuGVMp7vx7Eyi4ce1MuVg3k9NBe6k8aqfj0x8')
+  // .then((res: any) => console.log(res))
+  // .catch((error: any) => console.error(error));
+
+  this.twitter.login().then(onSuccess, onError);
+
+  function onSuccess(response) {
+    console.log(response);
+  
+    // Will console log something like:
+    // {
+    //   userName: 'myuser',
+    //   userId: '12358102',
+    //   secret: 'tokenSecret'
+    //   token: 'accessTokenHere'
+    // }
+  }
+
+  function onError(response) {
+    console.log(response);
+  
+    // Will console log something like:
+    // {
+    //   userName: 'myuser',
+    //   userId: '12358102',
+    //   secret: 'tokenSecret'
+    //   token: 'accessTokenHere'
+    // }
+  }
+  }
 }
+
+// cordova plugin add https://github.com/chroa/twitter-connect-plugin --variable FABRIC_KEY=803837273708101632-3JoY83cLxaYj3whBWLBbZWOy4eSOsuq --variable TWITTER_KEY=mMWzV0QUfCLYwQzbv9EOOi0ed --variable TWITTER_SECRET=AAn5TgqbT1uNN9dZK0XBLHATAUn09UmUbHEXt4Sc8j3zenPWTg
